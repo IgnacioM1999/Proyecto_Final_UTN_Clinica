5b94cb9fb7dacaec8bebe9d02774680f94f699dd
IgnacioMartinez <martinezignacio99@gmail.com>
2025-08-12 09:22:50 -0300

    Creacion del componente Administrador y definicion del panel principal del Administrador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login';
+import { Admin } from './pages/admin/admin';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    {path: '', redirectTo: '/login', pathMatch: 'full' },
+    {path: 'admin', component: Admin }
 ];
---
+++ src/app/pages/admin/admin.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin',
+  imports: [CommonModule],
+  templateUrl: './admin.html',
+  styleUrl: './admin.css'
+})
+
+export class Admin {
+
+  constructor(private router: Router){}
+
+  secciones = [
+    {nombre:'Gestionar Turnos' , ruta:''},
+    {nombre:'Gestionar Sesiones' , ruta:''},
+    {nombre:'Gestionar Especialistas' , ruta:''},
+    {nombre:'Gestionar Pasantes' , ruta:''},
+    {nombre:'Gestionar Pacientes' , ruta:''},
+    {nombre:'Gestionar Instrumentos' , ruta:''},
+    {nombre:'Historial Clinico' , ruta:''},
+  ]
+
+  salir(){
+    this.router.navigate(["/"]); //Navega a la ruta laiz, que seria el login
+  }
+
+}
